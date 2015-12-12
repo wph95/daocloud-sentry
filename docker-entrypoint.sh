e6d66bce4cd82777cd906927b7c2af7e2753f804
Peter Wang <wph95@codevs.com>
2015-12-12 15:43:15 +0800

    fix chmod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sentry --config=/daocloud-links.conf.py upgrade
+exec sentry --config=/daocloud-links.conf.py start
 exec sentry --config=/daocloud-links.conf.py celery beat
 exec sentry --config=/daocloud-links.conf.py celery worker
-sentry --config=/daocloud-links.conf.py start
-
